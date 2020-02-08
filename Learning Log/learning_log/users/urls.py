199f244abedf9d92a357cc21599ae8adf571e91e
maximdv34 <maximdv34@gmail.com>
2020-02-08 18:30:17 +0200

    commit with concole 2 c

--- Learning Log/learning_log/users/urls.py
+++ Learning Log/learning_log/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.login_, name='login')
+    path('register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
     # path('login/', include('django.contrib.auth.urls')),
     path('logout/', views.logout_view, name='logout')
