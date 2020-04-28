23130f4edcbccadfb09fed9745505df5d7ac6a7e
maximdv34 <maximdv34@gmail.com>
2020-04-28 10:23:16 +0300

    New branch

---
+++ Learning Log/learning_log/users/forms.py
@@ -0,0 +1,9 @@
+from .forms import LoginForm, UserRegistrationForm
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
--- Learning Log/learning_log/users/models.py
+++ Learning Log/learning_log/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.forms import forms
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
